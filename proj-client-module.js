2e51a8ff11c4911c1d90d10b04f48c53bf26561f
Alexandre Braga <alexandre_cgr@hotmail.com>
2016-09-14 13:47:03 -0400

    logout

--- proj-client-module.js
+++ proj-client-module.js
@@ -7,9 +7,26 @@ if (typeof localStorage === "undefined" || localStorage === null) {
 }
 
 function login(url, username, password) {
-    request.post(url).send({username: username, password: password}).end(function(err, res) {
+    request.post(url + '/login').send({username: username, password: password}).end(function(err, res) {
+	if(err) {
+	    localStorage.setItem('login-error', err); 
+	    return;
+	}
 	localStorage.setItem('token', res.text.split("\"")[3]);	
     });
 }
 
+function logout(url) {
+    request
+    .post(url + '/logout?access_token=' + localStorage.getItem('token'))
+    .end(function(err, res){
+	if(err) {
+	    localStorage.setItem('logout-error', err); 
+	    return;
+	}
+	localStorage.setItem('logout-status', res.status);
+    });
+}
+
 module.exports.login = login;
+module.exports.logout = logout;
